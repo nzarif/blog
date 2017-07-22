6c8cb4e8ad35d6bf92f9bad5af92a337c65656d2
niloofar <niloofar.zrf@gmail.com>
2017-07-22 19:10:08 +0430

    ish

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'l6nz&dh0i09(udf$zmq+47zlet5dlqp7-z3ae9^(5(6bh80-d_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mysterious-hollows-53430.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -132,6 +132,28 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR + "/static/",
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
